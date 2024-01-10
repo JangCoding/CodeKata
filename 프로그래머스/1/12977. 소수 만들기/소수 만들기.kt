2a8d0f1b2d2a8ab3a5dc44b87b77fd5acd77165e
JangCoding <62090021+JangCoding@users.noreply.github.com>
2024-01-10 20:05:48 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -2,7 +2,6 @@ class Solution {
     fun solution(nums: IntArray): Int {
         var answer = 0
 
-
         for(i in 0 until  nums.size - 2){
             for( j  in  i+1 until  nums.size - 1){
                 for( k in j+1 until  nums.size){
