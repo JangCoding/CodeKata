d63e5345f5a7614cd86b03d77d5a31acf32b127d
JangCoding <62090021+JangCoding@users.noreply.github.com>
2024-01-10 10:01:01 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun solution(nums: IntArray): Int {
+        var answer = 0
+
+
+        for(i in 0 until  nums.size - 2){
+            for( j  in  i+1 until  nums.size - 1){
+                for( k in j+1 until  nums.size){
+                    var sum = nums[i]+nums[j]+nums[k]
+
+                    for(l in 2..sum){
+                        if(sum == l) {
+                            answer += 1
+                        }
+                        else if(sum % l == 0)
+                            break
+                    }
+                }
+            }
+        }
+        return answer
+    }
+}
+
+fun main() {
+    var S=Solution()
+    println(S.solution(intArrayOf(1,2,7,6,4)))
+}
+
