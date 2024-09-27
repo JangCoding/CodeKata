56ecfdb8f73f204f9b16eb5b76f54e821ea1a6f9
JangCoding <62090021+JangCoding@users.noreply.github.com>
2024-09-27 12:18:28 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 57.46 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.kt
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.kt
@@ -1,78 +1,29 @@
-// 둘 중 선물 많이 준 사람 
-// 선물지수 = 준 선물 수 - 받은 선물 수
-// 만약 두 사람의 선물 지수도 같다면 다음 달에 선물을 주고받지 않습니다.
-
-// 표 2개 필요
-// 서로 준 선물 목록 (2차)
-// 총 준 선물 받은선물 선물지수 (2차)
-
-// 가장 많은 선물 받는 친구 받을 선물 수 return
-
 class Solution {
-    public fun solution(friends: Array<String>, gifts: Array<String>): Int {
-        var answer: Int = 0
+    fun solution(friends: Array<String>, gifts: Array<String>): Int {
+        // 선물 수량을 세기 위한 맵 생성
+        val giftMap = gifts.groupingBy { it }.eachCount()
+        // 각 친구의 선물 지수를 계산하여 맵에 저장
+        val pIdxMap = friends.map { it to pIdx(it, friends, giftMap) }.toMap()
+        
+        // 각 친구의 선물 지수를 계산하고, 최대값 반환
+        return friends.map {
+            numOfPresents(it, friends, giftMap, pIdxMap)
+        }.maxOrNull() ?: 0 // maxOrNull() 사용
+    }
 
-        var friendSize = friends.size
-        var giftTable = Array(friendSize){Array(friendSize){0}}
-        var giftPoint = Array(friendSize){Array(3){0}}
+    // 친구의 선물 지수 계산
+    fun pIdx(friend: String, friends: Array<String>, giftMap: Map<String, Int>): Int {
+        val give = friends.sumBy { giftMap["$friend $it"] ?: 0 } 
+        val take = friends.sumBy { giftMap["$it $friend"] ?: 0 }
+        return give - take
+    }
 
-        var nextGift = Array(friendSize){0}
-
-
-        // 선물 주고받은 횟수 기록 테이블
-        for( g in  gifts )
-        {
-            val giftTo = g.split(" ")
-            giftTable[friends.indexOf(giftTo[0])][friends.indexOf(giftTo[1])] += 1
+    // 특정 친구가 다른 친구들에게 줄 수 있는 선물의 수 계산
+    fun numOfPresents(friend: String, friends: Array<String>, giftMap: Map<String, Int>, pIdxMap: Map<String, Int>): Int {
+        return friends.count {
+            val give = giftMap["$friend $it"] ?: 0
+            val take = giftMap["$it $friend"] ?: 0
+            give > take || (give == take && pIdxMap[friend]!! > pIdxMap[it]!!)
         }
-
-        // 선물 지수 테이블
-        for ( i in 0 until friendSize)
-        {
-            // 준, 받은 선물 합산
-            for( j in 0 until friendSize)
-            {
-                giftPoint[i][0] += giftTable[i][j]
-                giftPoint[j][1] += giftTable[i][j]
-            }
-
-        }
-
-        for ( i in 0 until friendSize)
-        {
-            giftPoint[i][2] = giftPoint[i][0] - giftPoint[i][1]
-        }
-
-
-        // 주고받은 을 확인하고 테이블 지수를 확인하여 다음 달 받을 선물 수 계산
-        for ( i in 0 until friendSize)
-        {
-            // 준, 받은 선물 합산
-            for( j in i until friendSize)
-            {
-                if(i!=j) {
-                    if((giftTable[i][j] == 0 && giftTable[j][i] == 0) || (giftTable[i][j] == giftTable[j][i]) ){
-                        if (giftPoint[i][2] > giftPoint[j][2]) {
-                            nextGift[i] += 1
-                        } else if (giftPoint[i][2] < giftPoint[j][2]) {
-                            nextGift[j] += 1
-                        }
-                    }
-                    else if (giftTable[i][j] > giftTable[j][i]) {
-                        nextGift[i] += 1
-                    }
-                    else
-                        nextGift[j] += 1
-                }
-            }
-        }
-
-
-
-
-        print(giftTable)
-        print(giftPoint)
-
-        return nextGift.maxOf{it}
     }
 }
